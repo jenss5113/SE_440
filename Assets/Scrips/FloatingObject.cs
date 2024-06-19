584f2b4fd5c007e6970a3a7c57d698a499178c20
jenss5113 <114690721+jenss5113@users.noreply.github.com>
2024-06-19 20:40:08 +0700

    Demo_Under_Water_shader

---
+++ Assets/Scrips/FloatingObject.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class FloatingObject : MonoBehaviour
+{
+    [SerializeField] private float underWaterDrag;
+    [SerializeField] private float underWaterAngularDrag;
+    [SerializeField] private float airDrag;
+    [SerializeField] private float airAngularDrag;
+    [SerializeField] private float waterPower;
+    [SerializeField] private Transform[] floatPoints;
+
+    private Rigidbody _rb;
+    private bool _isUnderWater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int poinUnderWaterCount = 0;
+        foreach (var point in floatPoints)
+        {
+            var diff = point.position.y - 0;
+            if (diff < 0)
+            {
+                _rb.AddForceAtPosition(
+                    Vector3.up * waterPower * Mathf.Abs(diff),
+                    point.position,
+                    ForceMode.Acceleration);
+                poinUnderWaterCount++;
+                if (!_isUnderWater)
+                {
+                    _isUnderWater = true;
+                    Setstage(true);
+                }
+            }
+        }
+        if (_isUnderWater && poinUnderWaterCount == 0)
+        {
+            _isUnderWater = false;
+            Setstage(underWater: false);
+        }    
+           
+    }
+
+    private void Setstage(bool underWater)
+    {
+        if (underWater)
+        {
+            _rb.drag = underWaterDrag;
+            _rb.angularDrag = underWaterAngularDrag;
+        }    
+        else
+        {
+            _rb.drag = airDrag;
+            _rb.angularDrag = airAngularDrag;
+        }    
+    }    
+}
